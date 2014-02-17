848b9f47f1d0bc064a29ce40dbf006b4a9805712
Tommy Stanton <tommystanton@gmail.com>
2014-02-17 13:34:12 -0800

    Initial commit of web app
    - Add package.json
    - Add feature test
    - Add example Express web app with /hello route

---
+++ test/app.test.js
@@ -0,0 +1,30 @@
+var chai = require('chai');
+var sinon = require('sinon');
+
+chai.Assertion.includeStack = true;
+chai.Assertion.showDiff = false;
+chai.use(require('sinon-chai'));
+
+var expect = chai.expect;
+
+var myApp = require('../lib/myApp.js');
+var request = require('superagent');
+
+describe('Sample web app', function() {
+  var port = 8080;
+  var baseUrl = 'http://localhost:' + port;
+
+  before(function start(done) {
+    myApp.start(port, done);
+  });
+
+  after(myApp.stop);
+
+  it('should say hello at /hello', function(done) {
+    request.get(baseUrl + '/hello').end(function (err, res) {
+      expect(err).to.not.be.ok;
+      expect(res.text).to.equal('Hello world!');
+      done();
+    });
+  });
+});
